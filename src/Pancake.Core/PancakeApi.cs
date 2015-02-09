4282bbad774270875fdec105c81da23f8168496e
Mark Henke <gilligan128@gmail.com>
2015-02-08 21:39:18 -0500

    added nuget.exe

---
+++ src/Pancake.Core/PancakeApi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Pancake.Core
+{
+    public class PancakeApi
+    {
+        private readonly ResourceCatalog _catalog = new ResourceCatalog();
+
+        public void Configure(Action<ResourceConfig> configure)
+        {
+            configure(_catalog);
+        }
+
+        public void Serve()
+        {
+            foreach (var resourceSet in _catalog.Resources.GroupBy(r => r.GetType()))
+            {
+                var provider = _catalog.ProviderFor(resourceSet.Key);
+                var resources = resourceSet.ToArray();
+                var systemResources = provider.GetSystemResources(resources);
+                var matchingResourcePairs =
+                    resources.Join(systemResources, l => l.Name, r => r.Name, (should, system) => new {should, system})
+                        .ToArray();
+                var missingResources = resources.Except(matchingResourcePairs.Select(m => m.should));
+
+                foreach (var missingResource in missingResources.Where(r => r.Ensure == Ensure.Present))
+                {
+                    provider.Create(missingResource);
+                }
+
+                foreach (
+                    var matchingResource in
+                        matchingResourcePairs.Where(x => x.should.Ensure == Ensure.Present)
+                            .Where(x => !x.should.Equals(x.system)))
+                {
+                    provider.Synchronize(matchingResource.should, matchingResource.system);
+                }
+
+                foreach (
+                    var resource in matchingResourcePairs.Select(r => r.should).Where(r => r.Ensure == Ensure.Absent))
+                {
+                    provider.Destroy(resource);
+                }
+
+                provider.Flush();
+            }
+        }
+    }
+}
---
+++ src/Pancake.Core/ResourceConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Pancake.Core
+{
+    public interface ResourceConfig
+    {
+        void Resource(Resource resource);
+        void OptimizeWith(ResourceOptimizer resourceOptimizer);
+        void RegisterProvider<TResource>(ResourceProvider<TResource> resourceProvider) where TResource : Resource;
+    }
+}
